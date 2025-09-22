da32f269a2e94b51236bc99622d88d6959e99406
lukmanpratama~ <zoezta01@gmail.com>
2025-09-22 09:35:33 +0700

    add iframe warehouse claims

--- resources/views/pages/warehouse_claims.blade.php
+++ resources/views/pages/warehouse_claims.blade.php
@@ -53,13 +53,13 @@
     <!-- Fact End -->   
 
     <!-- In-frame Looker Studio -->
-        <div class="container-fluid">
+    <div class="container-fluid">
         <div class="embed-responsive embed-responsive-16by9">
             <div class="text-center p-5"><h1 class="text-secondary fw-bold d-block">ON PROGRESS</h1>
-                <!-- <iframe width="1500" height="900" src="https://lookerstudio.google.com/embed/reporting/75f78948-575c-4ef4-aca3-73cfa2a8eaeb/page/kGm6D" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe> -->
-            </div>
+                <iframe width="600" height="338" src="https://lookerstudio.google.com/embed/reporting/050a7d77-aabe-41ab-8cd2-cb14a2af5075/page/p_4oiy5xbgfd" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
         </div>
     </div>
+    
 
     <!-- About Start -->
     <div class="container-fluid py-5 my-5">
